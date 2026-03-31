63e045e3d24f0eb9ff7591f0c4284e0e2f390653
Robert Hambrock <roberthambrock@gmail.com>
2026-03-31 12:00:39 +0200

    polkadot-sdk: 2512-3 -> 2603

--- pkgs/polkadot-sdk/generic.nix
+++ pkgs/polkadot-sdk/generic.nix
@@ -20,13 +20,13 @@
 rustPlatform.buildRustPackage rec {
   inherit pname;
 
-  version = "2512-3";
+  version = "2603";
 
   src = fetchFromGitHub {
     owner = "Lederstrumpf";
     repo = "polkadot-sdk";
     rev = "force-portable-blake2_simd-${version}";
-    hash = "sha256-IkYFIaEAzsRWlb2Gdwh9uqgbKR9BG57H3xaFD/LRbEM=";
+    hash = "sha256-a69Zuv2lW1H1r0idj9QBpE2nqPjELmtwTTjuDyn7wn4=";
 
     # the build process of polkadot requires a .git folder in order to determine
     # the git commit hash that is being built and add it to the version string.
@@ -52,7 +52,7 @@ rustPlatform.buildRustPackage rec {
     ./picosimd-0.9.3.patch
   ];
 
-  cargoHash = "sha256-CJDdqUJCYnYXQuV0XsNXE3H28xl/TsoT9uQN+YsDmuI=";
+  cargoHash = "sha256-RExpaeahatihDZbi+dcwpWudnc34EUa/5gvKCXj9u+s=";
 
   buildType = "production";
   buildAndTestSubdir = target;
