6ca7bd0b3d4fbd4745d2bce743a5c633711ab7f6
André Silva <andrerfosilva@gmail.com>
2026-05-08 18:34:09 +0100

    shell: use mold on linux (x86_64/aarch64) and llvm.lld on darwin

--- shell.nix
+++ shell.nix
@@ -5,7 +5,6 @@
 }:
 
 let
-  mold = pkgs.wrapBintoolsWith { bintools = pkgs.mold; };
   channelPkgs = fenixPkgs.${channel};
   rust-toolchain = fenixPkgs.combine [
     (channelPkgs.withComponents [
@@ -19,27 +18,41 @@ let
     fenixPkgs.latest.rustfmt
     fenixPkgs.targets.wasm32-unknown-unknown.${channel}.rust-std
   ];
+  cargoLinker =
+    with pkgs;
+    let
+      mold = wrapBintoolsWith { bintools = pkgs.mold; };
+      rustTarget = stdenv.hostPlatform.rust.cargoEnvVarTarget;
+      rustflags =
+        if stdenv.isDarwin then
+          "-Clink-arg=-fuse-ld=${llvmPackages.lld}/bin/ld64.lld"
+        else
+          "-Clink-arg=-fuse-ld=${mold}/bin/ld.mold -Clink-arg=-Wl,--no-rosegment";
+    in
+    {
+      "CARGO_TARGET_${rustTarget}_LINKER" = "clang";
+      "CARGO_TARGET_${rustTarget}_RUSTFLAGS" = rustflags;
+    };
 in
 with pkgs;
-mkShell.override { stdenv = clangStdenv; } {
-  packages = [
-    llvmPackages.lld
-    openssl
-    pkg-config
-    rust-toolchain
-  ]
-  ++ lib.optionals stdenv.hostPlatform.isLinux [
-    rust-jemalloc-sys-unprefixed
-  ];
+mkShell.override { stdenv = clangStdenv; } (
+  {
+    packages = [
+      llvmPackages.lld
+      openssl
+      pkg-config
+      rust-toolchain
+    ]
+    ++ lib.optionals stdenv.hostPlatform.isLinux [
+      rust-jemalloc-sys-unprefixed
+    ];
 
-  # use mold as linker on linux x86_64
-  CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "clang";
-  CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-Clink-arg=-fuse-ld=${mold}/bin/ld.mold -Clink-arg=-Wl,--no-rosegment";
+    LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages.libclang ];
+    RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
 
-  LIBCLANG_PATH = lib.makeLibraryPath [ llvmPackages.libclang ];
-  RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
-
-  OPENSSL_NO_VENDOR = 1;
-  PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
-  ROCKSDB_LIB_DIR = lib.makeLibraryPath [ rocksdb ];
-}
+    OPENSSL_NO_VENDOR = 1;
+    PROTOC = "${lib.makeBinPath [ protobuf ]}/protoc";
+    ROCKSDB_LIB_DIR = lib.makeLibraryPath [ rocksdb ];
+  }
+  // cargoLinker
+)
